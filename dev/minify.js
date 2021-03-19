aa7f2f17370fce27f510b76d5e417627500aa381
LCordial <ebessen@internode.on.net>
2021-03-20 10:14:15 +1030

    minify css backend run command node dev/minify.js

---
+++ dev/minify.js
@@ -0,0 +1,23 @@
+// Minifying CSS using Uglifycss npm package
+// Run command when releasing for production
+
+// Command: node dev/minify.js
+
+var uglifycss = require('uglifycss');
+
+// File system
+var fs = require('fs');
+ 
+// Uglify API
+var uglified = uglifycss.processFiles(
+    [ './framework/ambience.css' ],
+    { maxLineLen: 500, expandVars: true }
+);
+ 
+console.log(uglified);
+
+// Writes in new file, creates new file if it doesn't exists. Throw error if something bad happened
+fs.appendFile('./framework/min/ambience.min.css', uglified, function (err) {
+    if(err) throw err;
+    console.log("Saved")
+});
