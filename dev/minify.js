87c401f176d0ca7a915f680bf4f0abeb3b61c8ce
ERieger <e.rieger04@gmail.com>
2021-04-07 14:41:05 +0930

    add custom npm commands - see main.scss

--- dev/minify.js
+++ dev/minify.js
@@ -3,21 +3,21 @@
 
 // Command: node dev/minify.js
 
-var uglifycss = require('uglifycss');
+var uglifycss = require("uglifycss");
 
 // File system
-var fs = require('fs');
- 
+var fs = require("fs");
+
 // Uglify API
-var uglified = uglifycss.processFiles(
-    [ './framework/ambience.css' ],
-    { maxLineLen: 500, expandVars: true }
-);
- 
+var uglified = uglifycss.processFiles(["./framework/ambience.css"], {
+  maxLineLen: 500,
+  expandVars: true,
+});
+
 console.log(uglified);
 
 // Writes in new file, creates new file if it doesn't exists. Throw error if something bad happened
-fs.appendFile('./framework/min/ambience.min.css', uglified, function (err) {
-    if(err) throw err;
-    console.log("Saved")
+fs.appendFile("./framework/min/ambience.min.css", uglified, function (err) {
+  if (err) throw err;
+  console.log("Saved");
 });
